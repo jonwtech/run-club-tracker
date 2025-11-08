0623e4b441dd2c7a2256799f2241f8a9e172e9cf
Jon Ward <jon@jonward.tech>
2025-11-08 11:07:03 +0000

    chore: add client id

---
+++ config.template.js
@@ -0,0 +1,6 @@
+// Configuration template for Run Club Tracker
+// Copy this file to config.js and fill in your actual values
+
+window.APP_CONFIG = {
+    STRAVA_CLIENT_ID: 'YOUR_STRAVA_CLIENT_ID'
+};
